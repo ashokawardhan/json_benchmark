0147c3415ceabf963de3ec57c4ae0a663388fb18
th3dark0n3 <ashokwardha@gmail.com>
2015-01-10 00:31:33 +0530

    Added both string and POJO comparison

--- src/jackson_json/TreeModeParser1.java
+++ src/jackson_json/TreeModeParser1.java
@@ -36,19 +36,13 @@ public class TreeModeParser1
 
 	public static void main(String[] args) throws MalformedURLException, IOException
 	{
-		//BufferedReader buf = new BufferedReader(new FileReader("small-dict.json"));
 		FileInputStream targetFile = new FileInputStream(new File("medium.json"));
-		// String url = "small.json";
 		// Get the contents of json as a string using commons IO IOUTils class.
 		String genreJson = IOUtils.toString(targetFile, "UTF-8"), temp;
-		//while ((temp = buf.readLine()) != null)
-		//{
-		//	genreJson += temp;
-		//}
 		
 		double jacksontime = 0.0, gsontime = 0.0,jacksontimeser = 0.0, gsontimeser = 0.0, gsontimecre = 0.0, jacksontimecre = 0.0, jsontimecre = 0.0, jsontimeser = 0.0, jsontimedes = 0.0, jacksontimeserstring = 0.0, jacksontimedeserstring = 0.0, gsontimeserstring = 0.0, gsontimedeserstring = 0.0;
 		long gsonsizeser = 0, gsonsizedes = 0, jacksonsizeser = 0, jacksonsizedes = 0, jsonsizeser = 0, jsonsizedes = 0,gsonsizeserstring = 0, gsonsizedeserstring = 0, jacksonsizeserstring = 0, jacksonsizedeserstring = 0;
-		//System.out.println(str);
+
 		for (int i = 0; i < iter; i++)
 		{
 			double lDateTimejsoncre = new Date().getTime();
@@ -62,7 +56,6 @@ public class TreeModeParser1
 			double lDateTimejacksoncre = new Date().getTime();
 			ObjectMapper mapper = new ObjectMapper();
 			double lDateTimeNewjacksoncre = new Date().getTime();
-			//mapper.registerModule(new AfterburnerModule());
 			Medium jack = mapper.readValue(genreJson, Medium.class);
 			double lDateTimejacksonser = new Date().getTime();
 			String jack1 = mapper.writeValueAsString(jack);
@@ -78,11 +71,8 @@ public class TreeModeParser1
 			JSONObject jsonObj = json.fromObject(genreJson);
 			double lDateTimeNewjsonser = new Date().getTime();
 			String json1 = jsonObj.toString();
-			//double lDateTimegson = new Date().getTime();
 			Medium str1 = gson.fromJson(genreJson, Medium.class);
 			JsonFactory factory = new JsonFactory();
-			
-			//double lDateTimeNewgson = new Date().getTime();
 			double lDateTimegsonser = new Date().getTime();
 			String str = gson.toJson(str1);
 			double lDateTimeNewgsonser = new Date().getTime();
@@ -105,12 +95,9 @@ public class TreeModeParser1
 			
 			
 			double lDateTimejsondes = new Date().getTime();
-			//JSONObject jsonObj1 = JSONObject.fromObject(json1);
 			Object json2 = JSONObject.fromObject(JSONObject.toBean(jsonObj, Medium.class));
 			double lDateTimeNewjsondes = new Date().getTime();
-			//System.out.println(str.substring(1));
-			
-			//System.out.println();
+
 			
 			if (i >= iter-500) {
 				gsonsizeser += str.getBytes().length;
@@ -169,36 +156,6 @@ public class TreeModeParser1
 				System.out.println("Deserializing size:"+jsonsizedes/(i-(iter-500)));
 				System.out.println("Deserializing time:"+jsontimeser/(i-(iter-500)));
 				System.out.println("Serializing time:"+jsontimedes/(i-(iter-500)));
-				
-				//System.out.println(jack2);
-			// lets see what type the node is
-			// System.out.println(node.getNodeType()); // prints OBJECT
-			// is it a container
-			// System.out.println(node.isContainerNode()); // prints true
-			// lets find out what fields it has
-			//Iterator<String> fieldNames = node.fieldNames();
-			// while (fieldNames.hasNext()) {
-			// String fieldName = fieldNames.next();
-			// System.out.println(fieldName);// prints title, message, errors,
-			// total,
-			// total_pages, page, limit, dataset
-			// }
-
-			// we now know what elemets the container has. lets get the value for
-			// one of them
-			// System.out.println(node.get("title").asText()); // prints
-			// "Free Music Archive".
-
-			// Lets look at the dataset now.
-			// JsonNode dataset = node.get("dataset");
-
-			// what is its type?
-			// System.out.println(node.getNodeType()); // Prints ARRAY
-
-			// so the dataset is an array. Lets iterate through the array and see
-			// what each of the elements are
-			//Iterator<JsonNode> datasetElements = node.iterator();
-			 //printer(datasetElements);
 			}
 		}
 	}
